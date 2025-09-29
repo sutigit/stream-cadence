99e9c588e3c4992ea706938626270821f3735fbf
sutigit <panyanilben@gmail.com>
2025-09-29 20:56:55 +0300

    create sandbox

--- apps/web/app/sandbox/page.tsx
+++ apps/web/app/sandbox/page.tsx
@@ -1,48 +1,30 @@
-'use client';
-
-import { useEffect, useRef, useState } from "react";
-import ChatInput from "../../components/chat-input";
-
-import "./cadence.css"
-import { useScroll } from "@/hooks/use-scroll";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-    const [input, setInput] = useState<string>("");
-    const [texts, setTexts] = useState()
-    const [loading, setLoading] = useState(false);
-    const scrollRef = useRef<HTMLDivElement>(null);
-    useScroll(scrollRef, texts)
-
-
-    async function onSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        if (!input.trim()) return;
-
-        setInput("");
-        setLoading(true);
-
-        try {
-
-
-        } finally {
-            setLoading(false);
-        }
-    }
 
 
     return (
-        <main className="container mx-auto h-screen max-h-screen min-h-screen flex">
-            <section className="px-20 flex-2 h-full flex flex-col justify-center items-center">
-                <div
-                    className="w-full flex mb-20 overflow-y-scroll h-[9rem] whitespace-pre-wrap pr-8 scroll-bar"
-                    ref={scrollRef}
-                >
-                    <p className="text-xl leading-9">
-                        Hello
-                    </p>
+        <main className="container mx-auto h-screen max-h-screen min-h-screen flex items-center justify-center">
+            <section>
+                <h1 className="text-4xl text-center">Sandbox</h1>
+                <div className="flex gap-5 mt-5">
+                    <Button className="rounded-full">
+                        <Link href="/sandbox/text-stream">text-stream</Link>
+                    </Button>
+
+                    <Button className="rounded-full">
+                        <Link href="/sandbox/completion-stream">completion-stream</Link>
+                    </Button>
+
+                    <Button className="rounded-full">
+                        <Link href="/sandbox/stored-completion-stream">stored-completion-stream</Link>
+                    </Button>
+
+                    <Button className="rounded-full">
+                        <Link href="/sandbox/conversation-stream">conversation-stream</Link>
+                    </Button>
                 </div>
-                <ChatInput onSubmit={onSubmit} input={input} setInput={setInput} />
             </section>
         </main>
     );
