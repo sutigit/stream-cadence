1f9a5f1545f36feecc155ab2c73bf968d162b5fd
sutigit <panyanilben@gmail.com>
2025-10-04 13:21:29 +0300

    move lib

--- apps/web/components/chat-input.tsx
+++ apps/web/components/chat-input.tsx
@@ -14,20 +14,6 @@ export default function ChatInput({
 
     return (
         <div className="w-full">
-            <div className="pb-3 flex gap-4 flex-wrap justify-start">
-                <Button className="cursor-pointer bg-zinc-800 text-primary/70 hover:opacity-100 px-6 py-3 rounded-full text-sm inline-flex items-center gap-2 whitespace-nowrap">
-                    <span className="truncate">Tell me about our second closest star</span>
-                    <span className="flex-shrink-0"><Send size={14} /></span>
-                </Button>
-                <Button className="cursor-pointer bg-zinc-800 text-primary/70 hover:opacity-100 px-6 py-3 rounded-full text-sm inline-flex items-center gap-2 whitespace-nowrap">
-                    <span className="truncate">Read me a bed time story</span>
-                    <span className="flex-shrink-0"><Send size={14} /></span>
-                </Button>
-                <Button className="cursor-pointer bg-zinc-800 text-primary/70 hover:opacity-100 px-6 py-3 rounded-full text-sm inline-flex items-center gap-2 whitespace-nowrap">
-                    <span className="truncate">How does this technology work?</span>
-                    <span className="flex-shrink-0"><Send size={14} /></span>
-                </Button>
-            </div>
             <form onSubmit={onSubmit} className="flex mx-auto gap-5 bg-zinc-800 rounded-3xl py-3 pl-8 pr-3">
                 <input
                     type="text"
