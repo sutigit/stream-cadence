fda9376a174972dae8de93a3c744010123e5e21d
sutigit <panyanilben@gmail.com>
2025-10-10 15:20:02 +0300

    update we ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -13,7 +13,6 @@ import { useScroll } from "@/hooks/use-scroll";
 import { fetchResponse } from "./api/openai/utils";
 import { useStreamNice } from "@/lib/StreamNice/hooks/useStreamNice";
 import { StreamConfig } from "@/lib/StreamNice/types";
-import { StreamNice } from "@/lib/StreamNice";
 
 
 export default function Home() {
@@ -33,78 +32,77 @@ export default function Home() {
 
   const { streamReader, segs, setSegs } = useStreamNice(options)
 
-  async function onSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    if (!input.trim()) return;
-
-    setInput("");
-    setLoading(true);
-    setSegs([])
-
-    const res = await fetchResponse(input);
-    if (!res?.body) throw new Error("No response body");
-    const reader = res.body.getReader();
-
-    await streamReader(reader, (next, done) => {
-      setSegs(prev => [...prev, next]);
-    })
-
-    try {
-
-    } finally {
-      setLoading(false);
-    }
-  }
 
   return (
-    <main className="container mx-auto h-screen max-h-screen min-h-screen flex">
-      <section className="px-20 flex-2 h-full flex flex-col justify-center items-center">
-        <div
-          className="w-full flex mb-20 overflow-y-scroll h-[9rem] whitespace-pre-wrap pr-8 scroll-bar"
-          ref={scrollRef}
-        >
-          <StreamNice segs={segs} className="text-2xl leading-9"></StreamNice>
+    <main className="container mx-auto py-20 h-screen max-h-screen min-h-screen">
+      <div className="mb-10">
+        <p className="text-6xl font-bold mb-2">Customize AI message</p>
+        <p className="text-6xl font-bold">streams with ease.</p>
+      </div>
+      <div className="w-full h-2/3 grid grid-cols-3 gap-1">
+
+        {/* Chat container --------------------- */}
+        <div className="chat-container w-full h-full px-6 py-4 col-span-2 bg-orange-100/80 rounded-2xl grid place-items-center">
+          <div className="w-full h-full flex flex-col justify-between">
+            <p className="text-primary-foreground font-bold text-lg mb-3">Your AI message</p>
+            <div className="h-15 w-full bg-orange-200/50 shadow-md rounded-full flex flex-row justify-between items-center pl-6 pr-3">
+              <p className="italic text-primary-foreground/40">Type your message here</p>
+              <div className="h-10 aspect-square rounded-full bg-indigo-200"></div>
+            </div>
+          </div>
         </div>
-        <ChatInput onSubmit={onSubmit} input={input} setInput={setInput} />
-      </section>
 
-      <aside className="flex-1 h-full flex justify-center items-center">
-        <section className="border-l-4 border-zinc-800/70 py-5 px-15 flex flex-col gap-14">
+        {/* Chat settings ---------------------- */}
+        <div className="chat-settings w-full h-full grid col-span-1 grid-rows-4 gap-1">
 
-          <div>
-            <p className="mb-4 opacity-50 flex items-center gap-2">
-              Streaming speed
-              <Rabbit size={18} />
-            </p>
-            <div className="flex items-center gap-3">
-              <Button onClick={() => setReadSpeed('slow')} className={cn("rounded-full cursor-pointer px-5", readSpeed === 'slow' ? 'bg-zinc-100' : 'bg-zinc-400')}>Slow</Button>
-              <Button onClick={() => setReadSpeed('average')} className={cn("rounded-full cursor-pointer px-5", readSpeed === 'average' ? 'bg-zinc-100' : 'bg-zinc-400')}>Average</Button>
-              <Button onClick={() => setReadSpeed('fast')} className={cn("rounded-full cursor-pointer px-5", readSpeed === 'fast' ? 'bg-zinc-100' : 'bg-zinc-400')}>Fast</Button>
-              <Button onClick={() => setReadSpeed('custom')} className={cn("rounded-full cursor-pointer px-5", readSpeed === 'custom' ? 'bg-zinc-100' : 'bg-zinc-400')}>Custom</Button>
+
+          {/* system prompt ----------------------*/}
+          <div className="system-prompt px-6 py-4 w-full h-full bg-lime-100/80 row-span-2 rounded-2xl grid place-items-center">
+            <div className="w-full h-full">
+              <p className="text-primary-foreground font-bold text-xl mb-4">System prompt</p>
+              <p className="text-primary-foreground/80 leading-5 whitespace-pre-line">
+                {
+                  `Prefix all the key stuff regarding the topic with !key:
+
+                  e.g. 
+                  Lorem !key:ipsum`
+                }
+              </p>
             </div>
           </div>
 
-          <div>
-            <div className="flex justify-between items-center">
-              <p className="mb-4 opacity-50 flex items-center gap-2 w-full">Advanced settings<Settings2 size={18} /><Copy className="cursor-pointer ml-auto" size={16} /></p>
+          {/* Stream cadence and speed -------------- */}
+          <div className="stream-cadence-and-speed row-span-1 grid grid-cols-3 gap-1">
+            <div className="stream-speed-slider px-6 py-4 bg-stone-100/80 rounded-2xl col-span-2 grid place-items-center">
+              <div className="w-full h-full">
+                <p className="text-primary-foreground font-bold text-lg mb-3">Streaming speed</p>
+              </div>
             </div>
-            <div className="flex flex-col gap-7 w-full">
-              <div>
-                <p className="opacity-50 mb-3 text-sm">Characters per second: {charsPerSec}</p>
-                <Slider defaultValue={[charsPerSec]} max={100} step={1} onValueChange={(values) => setCharsPerSec((values[0] ?? 1))} />
-              </div>
-              <div>
-                <p className="opacity-50 mb-3 text-sm">Short pause: {shortPause} ms</p>
-                <Slider defaultValue={[shortPause]} max={1000} step={1} onValueChange={(values) => setShortPause((values[0] ?? 1))} />
-              </div>
-              <div>
-                <p className="opacity-50 mb-3 text-sm">Long pause: {longPause} ms</p>
-                <Slider defaultValue={[longPause]} max={1000} step={1} onValueChange={(values) => setLongPause((values[0] ?? 1))} />
+            <div className="cadence-switch px-6 py-4 bg-red-100/80 rounded-2xl col-span-1 grid place-items-center">
+              <div className="w-full h-full">
+                <p className="text-primary-foreground font-bold text-lg mb-3">Cadence</p>
               </div>
             </div>
           </div>
-        </section>
-      </aside>
+
+          {/* Styled and components ----------------- */}
+          <div className="stream-switches row-span-1 grid grid-cols-2 gap-1">
+            <div className="switch-highlighs px-6 py-4 bg-purple-100/80 rounded-2xl grid place-items-center">
+              {/* highlights switch */}
+              <div className="w-full h-full">
+                <p className="text-primary-foreground font-bold text-lg mb-3">Highlights</p>
+              </div>
+            </div>
+            <div className="switch-interactions px-6 py-4 bg-emerald-100/80 rounded-2xl grid place-items-center">
+              {/* interactions switch */}
+              <div className="w-full h-full">
+                <p className="text-primary-foreground font-bold text-lg mb-3">Interactions</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
   );
 }
